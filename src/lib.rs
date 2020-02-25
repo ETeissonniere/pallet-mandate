f60d4197b7dca6f52b6934f8e17deb0ad65edcf4
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-02-25 11:20:57 -0800

    add events

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! can use the `apply` function to dispatch calls as root. Think of this module as an
 //! other `sudo` module controlled by another module (ex: a multisig or collective).
 
-use frame_support::{decl_module, dispatch::DispatchResult, Parameter};
+use frame_support::{decl_event, decl_module, dispatch::DispatchResult, Parameter};
 use sp_runtime::{
     traits::{Dispatchable, EnsureOrigin},
     DispatchError,
@@ -13,6 +13,7 @@ use sp_std::prelude::Box;
 
 /// The module's configuration trait.
 pub trait Trait: system::Trait {
+    type Event: From<Event> + Into<<Self as system::Trait>::Event>;
     type Proposal: Parameter + Dispatchable<Origin = Self::Origin>;
 
     /// Origin that can call this module and execute sudo actions. Typically
@@ -20,14 +21,23 @@ pub trait Trait: system::Trait {
     type ExternalOrigin: EnsureOrigin<Self::Origin>;
 }
 
+decl_event!(
+    pub enum Event {
+        /// A root operation was executed, show result
+        RootOp(bool),
+    }
+);
+
 decl_module! {
     /// The module declaration.
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        fn deposit_event() = default;
+
         pub fn apply(origin, proposal: Box<T::Proposal>) -> DispatchResult {
             T::ExternalOrigin::ensure_origin(origin)?;
 
             // Shamelessly stollen from the `sudo` module
-            match proposal.dispatch(system::RawOrigin::Root.into()) {
+            let res = match proposal.dispatch(system::RawOrigin::Root.into()) {
                 Ok(_) => true,
                 Err(e) => {
                     let e: DispatchError = e.into();
@@ -36,6 +46,7 @@ decl_module! {
                 }
             };
 
+            Self::deposit_event(Event::RootOp(res));
             Ok(())
         }
     }

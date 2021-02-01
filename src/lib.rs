5215acb4c959edebb7b12c45d24074d2790d95bb
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-02-01 10:45:24 +0100

    compile against rococo-v1 branch

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ use sp_runtime::{traits::Dispatchable, DispatchResult};
 use sp_std::prelude::Box;
 
 /// The module's configuration trait.
-pub trait Trait: system::Trait {
-    type Event: From<Event> + Into<<Self as system::Trait>::Event>;
+pub trait Config: system::Config {
+    type Event: From<Event> + Into<<Self as system::Config>::Event>;
     type Call: Parameter + Dispatchable<Origin = Self::Origin> + GetDispatchInfo;
 
     /// Origin that can call this module and execute sudo actions. Typically
@@ -30,12 +30,12 @@ decl_event!(
 
 decl_module! {
     /// The module declaration.
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+    pub struct Module<T: Config> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
         /// Let the configured origin dispatch a call as root
         #[weight = (call.get_dispatch_info().weight + 10_000, call.get_dispatch_info().class)]
-        pub fn apply(origin, call: Box<<T as Trait>::Call>) {
+        pub fn apply(origin, call: Box<<T as Config>::Call>) {
             T::ExternalOrigin::ensure_origin(origin)?;
 
             // Shamelessly stollen from the `sudo` module

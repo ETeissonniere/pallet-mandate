a017d6e33acf52d423a02de224199a7712f128f6
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-03-05 14:51:49 +0100

    bump deps to support substrate 3.0

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,12 @@
 use frame_support::{
     decl_event, decl_module, traits::EnsureOrigin, weights::GetDispatchInfo, Parameter,
 };
-use frame_system as system;
 use sp_runtime::{traits::Dispatchable, DispatchResult};
 use sp_std::prelude::Box;
 
 /// The module's configuration trait.
-pub trait Trait: system::Trait {
-    type Event: From<Event> + Into<<Self as system::Trait>::Event>;
+pub trait Config: frame_system::Config {
+    type Event: From<Event> + Into<<Self as frame_system::Config>::Event>;
     type Call: Parameter + Dispatchable<Origin = Self::Origin> + GetDispatchInfo;
 
     /// Origin that can call this module and execute sudo actions. Typically
@@ -30,12 +29,12 @@ decl_event!(
 
 decl_module! {
     /// The module declaration.
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+    pub struct Module<T: Config> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
         /// Let the configured origin dispatch a call as root
         #[weight = (call.get_dispatch_info().weight + 10_000, call.get_dispatch_info().class)]
-        pub fn apply(origin, call: Box<<T as Trait>::Call>) {
+        pub fn apply(origin, call: Box<<T as Config>::Call>) {
             T::ExternalOrigin::ensure_origin(origin)?;
 
             // Shamelessly stollen from the `sudo` module

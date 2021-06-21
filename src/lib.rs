ae4c47e4b3272d68277f747537e1a6b658784364
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-06-21 11:46:40 +0200

    use frame v2 syntax

--- src/lib.rs
+++ src/lib.rs
@@ -4,43 +4,53 @@
 //! can use the `apply` function to dispatch calls as root. Think of this module as an
 //! other `sudo` module controlled by another module (ex: a multisig or collective).
 
-use frame_support::{
-    decl_event, decl_module, traits::EnsureOrigin, weights::GetDispatchInfo, Parameter,
-};
-use sp_runtime::{traits::Dispatchable, DispatchResult};
-use sp_std::prelude::Box;
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::{dispatch::Dispatchable, pallet_prelude::*, weights::GetDispatchInfo};
+    use frame_system::pallet_prelude::*;
+    use sp_runtime::DispatchResult;
+    use sp_std::prelude::Box;
 
-/// The module's configuration trait.
-pub trait Config: frame_system::Config {
-    type Event: From<Event> + Into<<Self as frame_system::Config>::Event>;
-    type Call: Parameter + Dispatchable<Origin = Self::Origin> + GetDispatchInfo;
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        type Call: Parameter + Dispatchable<Origin = Self::Origin> + GetDispatchInfo;
 
-    /// Origin that can call this module and execute sudo actions. Typically
-    /// the `collective` module.
-    type ExternalOrigin: EnsureOrigin<Self::Origin>;
-}
+        /// Origin that can call this module and execute sudo actions. Typically
+        /// the `collective` module.
+        type ExternalOrigin: EnsureOrigin<Self::Origin>;
+    }
 
-decl_event!(
-    pub enum Event {
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
         /// A root operation was executed, show result
         RootOp(DispatchResult),
     }
-);
 
-decl_module! {
-    /// The module declaration.
-    pub struct Pallet<T: Config> for enum Call where origin: T::Origin {
-        fn deposit_event() = default;
-
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
         /// Let the configured origin dispatch a call as root
-        #[weight = (call.get_dispatch_info().weight + 10_000, call.get_dispatch_info().class)]
-        pub fn apply(origin, call: Box<<T as Config>::Call>) {
+        #[pallet::weight(call.get_dispatch_info().weight + 10_000)]
+        pub fn apply(
+            origin: OriginFor<T>,
+            call: Box<<T as Config>::Call>,
+        ) -> DispatchResultWithPostInfo {
             T::ExternalOrigin::ensure_origin(origin)?;
 
             // Shamelessly stollen from the `sudo` module
             let res = call.dispatch(frame_system::RawOrigin::Root.into());
 
             Self::deposit_event(Event::RootOp(res.map(|_| ()).map_err(|e| e.error)));
+
+            Ok(().into())
         }
     }
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 }
